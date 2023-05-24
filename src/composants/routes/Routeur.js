9904553ef001237349974c596fe3d0459b49bdff
Clement-Julia <81628168+Clement-Julia@users.noreply.github.com>
2023-05-24 16:56:41 +0200

    feat: add home

--- src/composants/routes/Routeur.js
+++ src/composants/routes/Routeur.js
@@ -2,17 +2,16 @@ import React from 'react'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import Navigation from '../layout/navigation'
 
-import Home from '../pages/Home';
+import Home from '../pages/Home'
 import NotFound from './error-page'
 
 const Routeur = () => {
 	return (
 		<BrowserRouter>
-            <Navigation/>
-                <Routes>
-                    <Route exact path='/' element={<Home/>} />
-                    <Route path='*' element={<NotFound />}/>
-                </Routes>
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route path='*' element={<NotFound />} />
+			</Routes>
 		</BrowserRouter>
 	)
 }
